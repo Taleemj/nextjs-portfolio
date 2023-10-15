1d9f0dfcd978f569e9cff2884293119ce3e27c7e
Taleem <taleemmankuer@gmail.com>
2023-10-15 12:52:07 -0700

    add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.scss";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Taleem | Web developer",
@@ -9,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
