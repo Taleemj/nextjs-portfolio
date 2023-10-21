e6835367cafee949ce6c320c4288252dc56b50b5
Taleem <taleemmankuer@gmail.com>
2023-10-21 04:50:56 -0700

    update to dynamic about content from sanity

--- components/aboutme/Aboutme.jsx
+++ components/aboutme/Aboutme.jsx
@@ -5,15 +5,22 @@ import RevealIt from "../RevealIt";
 import { client } from "@/sanity/lib/client";
 import { groq } from "next-sanity";
 import { urlForImage } from "@/sanity/lib/image";
+import { PortableText } from "@portabletext/react";
+import { RichText } from "../Richtext/Richtext";
 
 const Aboutme = async () => {
   const titlearr = ["A", "b", "o", "u", "t", " ", "M", "e"];
   const query = groq`
   *[_type == "about"]
   `;
+  const query1 = groq`
+  *[_type == "about"]{
+    "cvurl": CV.asset->url
+  }
+  `;
   const aboutstuff = await client.fetch(query);
-
-  console.log(aboutstuff);
+  const thecvUrl = await client.fetch(query1);
+  console.log(thecvUrl);
   return (
     <div id="aboutme" className={styles.about}>
       <RevealIt>
@@ -26,27 +33,8 @@ const Aboutme = async () => {
       <RevealIt>
         <div className={styles.aboutcontainer}>
           <div className={styles.txt}>
-            <p className="wow animate__animated animate__fadeInUp">
-              Hi, my name is <strong>Taleem Mankuer</strong> and i am an
-              aspiring <span style={{ color: "var(--cyan)" }}>developer</span>{" "}
-              and student with a good understanding in{" "}
-              <span style={{ color: "var(--cyan)" }}>web development</span>. I
-              enjoy finding creative solutions to problems and spend my time
-              experimenting with{" "}
-              <span style={{ color: "var(--cyan)" }}>technologies</span> such as
-              <span style={{ color: "var(--cyan)" }}> react</span> and inhale a
-              wide variety of potentially useful information through different
-              platforms.
-            </p>
-            <br />
-            <p className="wow animate__animated animate__fadeInUp">
-              I enjoy making conections and trying out new things, i love to
-              increase my skills and knowledge and would love to advance in my
-              career as a developer. I am open for any
-              <span style={{ color: "var(--cyan)" }}> opportunities</span>. I
-              build websites that delight and inform and try give it my all.
-            </p>
-            <a href={"/assets/TaleemCV.pdf"} target="_blank" rel="noreferrer">
+            <PortableText value={aboutstuff[0].bio} components={RichText} />
+            <a href={`${thecvUrl[0].cvurl}`} target="_blank" rel="noreferrer">
               <button className="wow animate__animated animate__fadeInUp">
                 Download CV
               </button>
