a7b937c03d461395c33396218b8c7db0120690f0
Taleem <taleemmankuer@gmail.com>
2023-10-21 04:49:39 -0700

    update

--- components/RevealIt.js
+++ components/RevealIt.js
@@ -2,8 +2,12 @@
 import React from "react";
 import { RevealWrapper } from "next-reveal";
 
-const RevealIt = ({ children }) => {
-  return <RevealWrapper reset={true}>{children}</RevealWrapper>;
+const RevealIt = ({ children, styles }) => {
+  return (
+    <RevealWrapper className={styles} reset={true}>
+      {children}
+    </RevealWrapper>
+  );
 };
 
 export default RevealIt;
