c094c1abb7959e5c91aa6ae8b43b01297b83bd21
Taleem <taleemmankuer@gmail.com>
2023-10-28 15:40:07 +0300

    optimze local fonts with next/font

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.scss";
+import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
@@ -7,9 +8,20 @@ export const metadata = {
     "Hi, my name is Taleem Mankuer and i am an aspiring web developer and student with a good understanding in web development. I enjoy finding creative solutions to problems and spend my time experimenting with technologies such as react and inhale a wide variety of potentially useful information through different platforms.",
 };
 
+const Roboto = localFont({
+  src: "../public/assets/fonts/Roboto Light V2.woff2",
+  display: "swap",
+  variable: "--roboto",
+});
+const Coolvetica = localFont({
+  src: "../public/assets/fonts/CoolveticaRg-Regular.woff2",
+  display: "swap",
+  variable: "--coolvetica",
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${Roboto.variable} ${Coolvetica.variable}`}>
       <head>
         <meta
           name="google-site-verification"
